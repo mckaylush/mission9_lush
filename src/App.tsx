c2a8949ddcd7f52dae9892257f1499cae5d76718
mckaylush <130932201+mckaylush@users.noreply.github.com>
2024-03-05 18:13:42 -0700

    Hw 9 Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+interface TeamCard {
+  name: string;
+  mascot: string;
+  location: string;
+}
+
+const teamName = [
+  {
+    name: "The Chicago Bulls",
+    mascot: "Bull",
+    location: "Chicago",
+  },
+];
+
+class Teams extends React.Component<TeamCard> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.name}</h2>
+        <h3>Members: {oneTeam.mascot}</h3>
+        <h3>Formed: {oneTeam.location}</h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamName.map((teamNum) => (
+        <Teams {...teamNum} />
+      ))}
+    </div>
+  );
+}
+
+function Welcome() {
+  return <h1>Welcome to March Madness!!</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
